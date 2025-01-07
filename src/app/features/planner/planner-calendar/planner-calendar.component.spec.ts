264ebf029cda5b80cab4a52ce4aa587919f816d3
Rocio Garrido <rocioganon@gmail.com>
2025-01-07 11:51:27 +0100

    feat: create components

---
+++ src/app/features/planner/planner-calendar/planner-calendar.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PlannerCalendarComponent } from './planner-calendar.component';
+
+describe('PlannerCalendarComponent', () => {
+  let component: PlannerCalendarComponent;
+  let fixture: ComponentFixture<PlannerCalendarComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [PlannerCalendarComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(PlannerCalendarComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/features/planner/planner-calendar/planner-calendar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-planner-calendar',
+  standalone: true,
+  imports: [],
+  templateUrl: './planner-calendar.component.html',
+  styleUrl: './planner-calendar.component.scss'
+})
+export class PlannerCalendarComponent {
+
+}
