191b5fb49bcd4de3fc6fad9d0d1c32ec85ecb219
Rocio Garrido <rocioganon@gmail.com>
2025-01-07 12:32:11 +0100

    feat: add routes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,20 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, Routes } from '@angular/router';
+import { RecipeListComponent } from './features/recipes/recipe-list/recipe-list.component';
+import { PlannerCalendarComponent } from './features/planner/planner-calendar/planner-calendar.component';
+import { ShoppingListComponent } from './features/shopping-list/shopping-list/shopping-list.component';
 
-import { routes } from './app.routes';
+const routes: Routes = [
+  { path: 'recipes', component: RecipeListComponent },
+  { path: 'planner', component: PlannerCalendarComponent },
+  { path: 'shopping-list', component: ShoppingListComponent },
+  { path: '', redirectTo: '/recipes', pathMatch: 'full' },
+  { path: '**', component: RecipeListComponent },
+];
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+  ],
 };
