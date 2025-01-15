5274ca13ec360b868960b8e3e8a69b355d64b562
Rocio Garrido <rocioganon@gmail.com>
2025-01-15 16:12:23 +0100

    feat: implement logic to open a date picker dialog

--- src/app/features/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/features/recipes/recipe-detail/recipe-detail.component.ts
@@ -9,6 +9,9 @@ import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatListModule } from '@angular/material/list';
+import { MatDialog } from '@angular/material/dialog';
+import { PlannerService } from '../../../data-access/services/planner.service';
+import { AddToPlannerDialogComponent } from '../../planner/add-to-planner-dialog/add-to-planner-dialog.component';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -26,12 +29,28 @@ import { MatListModule } from '@angular/material/list';
 export class RecipeDetailComponent {
   recipe$!: Observable<Recipe | undefined>;
 
-  constructor(private route: ActivatedRoute, private store: Store<AppState>) {}
+  constructor(private route: ActivatedRoute, private store: Store<AppState>, private dialog: MatDialog, private plannerService: PlannerService) {}
 
   ngOnInit(): void {
     const id = this.route.snapshot.paramMap.get('id');
     this.recipe$ = this.store
       .select(selectAllRecipes)
       .pipe(map((recipes) => recipes.find((recipe) => recipe.id === id)));
+  }
+
+  openAddToPlannerDialog(recipe: Recipe): void {
+    const dialogRef = this.dialog.open(AddToPlannerDialogComponent, {
+      data: { recipeName: recipe.name },
+    });
+
+    dialogRef.afterClosed().subscribe((selectedDate) => {
+      if (selectedDate) {
+        const formattedDate = selectedDate.toISOString().split('T')[0];
+        this.plannerService.addRecipeToPlanner(formattedDate, recipe.name);
+        console.log(
+          `La receta "${recipe.name}" se ha añadido al planner para el día ${formattedDate}.`
+        );
+      }
+    });
   }
 }
