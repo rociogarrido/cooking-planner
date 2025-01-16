40ead95497fddb19cc750e1e8dd982e59f28650c
Rocio Garrido <rocioganon@gmail.com>
2025-01-16 13:17:01 +0100

    feat: implement logic to display recipe cards inside the weekly planner for each day

--- src/app/features/planner/planner-calendar/planner-calendar.component.ts
+++ src/app/features/planner/planner-calendar/planner-calendar.component.ts
@@ -1,12 +1,60 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { MatTableModule } from '@angular/material/table';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { PlannerService } from '../../../data-access/services/planner.service';
+import { MatCardModule } from '@angular/material/card';
+import { Recipe } from '../../../data-access/models/recipe.model';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-planner-calendar',
   standalone: true,
-  imports: [],
+  imports: [MatTableModule, CommonModule, MatGridListModule, MatCardModule, RouterLink],
   templateUrl: './planner-calendar.component.html',
-  styleUrl: './planner-calendar.component.scss'
+  styleUrl: './planner-calendar.component.scss',
 })
-export class PlannerCalendarComponent {
+export class PlannerCalendarComponent implements OnInit {
+  displayedColumns: string[] = ['day', 'recipes'];
+  week: { day: string; recipes: Recipe[] }[] = [];
 
+  constructor(private plannerService: PlannerService) {}
+
+  ngOnInit(): void {
+    this.initializeWeek();
+    this.week = this.plannerService.getPlanner();
+  }
+
+  initializeWeek(): void {
+    const daysOfWeek = [
+      'Monday',
+      'Tuesday',
+      'Wednesday',
+      'Thursday',
+      'Friday',
+      'Saturday',
+      'Sunday',
+    ];
+    const today = new Date();
+    const currentDayIndex = today.getDay() - 1;
+    const startOfWeek = new Date(today);
+    startOfWeek.setDate(today.getDate() - currentDayIndex);
+
+    this.week = daysOfWeek.map((day, index) => {
+      const date = new Date(startOfWeek);
+      date.setDate(startOfWeek.getDate() + index);
+
+      return {
+        day: `${day}`,
+        recipes: [],
+      };
+    });
+  }
+
+  addRecipeToDay(day: string, recipe: Recipe): void {
+    const dayEntry = this.week.find((d) => d.day.startsWith(day));
+    if (dayEntry) {
+      dayEntry.recipes.push(recipe);
+    }
+  }
 }
