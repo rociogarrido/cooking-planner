d06861bb09212cbca6c943cccab5595b6deeb8eb
Rocio Garrido <rocioganon@gmail.com>
2025-01-23 16:34:54 +0100

    feat: integrate shopping list component with the service

--- src/app/features/shopping-list/shopping-list/shopping-list.component.ts
+++ src/app/features/shopping-list/shopping-list/shopping-list.component.ts
@@ -1,12 +1,29 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Ingredient } from '../../../data-access/models/ingredient.model';
+import { ShoppingListService } from '../../../data-access/services/shopping-list.service';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-shopping-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MatListModule, MatIconModule, MatButtonModule],
   templateUrl: './shopping-list.component.html',
-  styleUrl: './shopping-list.component.scss'
+  styleUrl: './shopping-list.component.scss',
 })
 export class ShoppingListComponent {
+  shoppingList: Ingredient[] = [];
 
+  constructor(private shoppingListService: ShoppingListService) {}
+
+  ngOnInit(): void {
+    this.shoppingList = this.shoppingListService.getShoppingList();
+  }
+
+  clearList(): void {
+    this.shoppingListService.clearShoppingList();
+    this.shoppingList = [];
+  }
 }
