9aee67c0698a88ff64285c2c822abc6118cc8958
Rocio Garrido <rocioganon@gmail.com>
2025-01-29 11:40:44 +0100

    feat: implement solution to export shopping list to pdf

--- src/app/features/shopping-list/shopping-list/shopping-list.component.ts
+++ src/app/features/shopping-list/shopping-list/shopping-list.component.ts
@@ -5,11 +5,20 @@ import { ShoppingListService } from '../../../data-access/services/shopping-list
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import jsPDF from 'jspdf';
+import autoTable from 'jspdf-autotable';
 
 @Component({
   selector: 'app-shopping-list',
   standalone: true,
-  imports: [CommonModule, MatListModule, MatIconModule, MatButtonModule],
+  imports: [
+    CommonModule,
+    MatListModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+  ],
   templateUrl: './shopping-list.component.html',
   styleUrl: './shopping-list.component.scss',
 })
@@ -25,5 +34,23 @@ export class ShoppingListComponent implements OnInit {
   clearList(): void {
     this.shoppingListService.clearShoppingList();
     this.shoppingList = [];
+  }
+
+  exportShoppingListToPDF(): void {
+    const doc = new jsPDF();
+
+    doc.text('Shopping List', 14, 10);
+
+    autoTable(doc, {
+      startY: 20,
+      head: [['Quantity', 'Unit', 'Ingredient']],
+      body: this.shoppingList.map((item) => [
+        item.quantity,
+        item.unit,
+        item.name,
+      ]),
+    });
+
+    doc.save('shopping-list.pdf');
   }
 }
